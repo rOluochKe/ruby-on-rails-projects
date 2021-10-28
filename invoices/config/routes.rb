c18c81204c415121f0bcedd62af20e922bb88e6b
roluochKe <rasholuoch@gmail.com>
2021-10-28 19:13:15 +0300

    updating contacts and accounts

--- invoices/config/routes.rb
+++ invoices/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # devise_for :users
   namespace :v1, defaults: {format: :json} do
-    resources :contacts
+    resources :accounts, only: [:create, :update] do
+      resources :contacts
+    end
     resources :sessions, only: [:create, :destroy]
     resources :users, only: [:create]
   end
