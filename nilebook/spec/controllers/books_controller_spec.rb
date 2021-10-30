26cf8811214b89ecfd8f30f627754e5d834121e7
roluochKe <rasholuoch@gmail.com>
2021-10-30 11:22:54 +0300

    active jobs and its tests

--- nilebook/spec/controllers/books_controller_spec.rb
+++ nilebook/spec/controllers/books_controller_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Api::V1::BooksController, type: :controller do
       get :index, params: { limit: 999 }
     end
   end
+
+  describe 'POST /create' do
+    let(:book_name) { 'Harry potter' }
+
+    it 'calls UpdateSkuJob with correct params' do
+      expect(UpdateSkuJob).to receive(:perform_later).with(book_name)
+
+      post :create, params: {
+        author: {first_name: 'JK', last_name: 'Rowling', age: 48},
+        book: {title: book_name}
+      }
+    end
+  end
 end
